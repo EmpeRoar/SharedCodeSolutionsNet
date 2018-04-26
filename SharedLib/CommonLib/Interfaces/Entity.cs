8e14128a1632ba07d4bdb12d3a9fe9cc915bbb3a
juliusbacosa <juliusbacosa@gmail.com>
2018-04-26 10:32:01 +0800

    Init

---
+++ SharedLib/CommonLib/Interfaces/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLib.Interfaces
+{
+    public class Entity : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime Deleted { get; set; }
+    }
+}
---
+++ SharedLib/CommonLib/Objects/Person.cs
@@ -0,0 +1,13 @@
+﻿using CommonLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLib.Objects
+{
+    public class Person : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
